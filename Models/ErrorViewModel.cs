3a5231b6d30266e8c616c14786547ebcb93069d5
Winsome Tang <tangwins@sheridancollege.ca>
2023-11-18 19:20:41 -0500

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Life_Liberator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
