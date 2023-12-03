b848a1e29c1f0de429302d0585ef603ba1d35530
hi8185 <coljacob@sheridancollege.ca>
2023-12-02 21:22:03 -0500

    Revert "NO MORE API! updated dashboard UI, added logo"
    This reverts commit 8333b8d07436bb2ff8c678ae1cfde4be6de71eb8.

---
+++ UserAPI/Models/DTOs/AddUserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserAPI.Models.DTO;
+
+public class AddUserDTO
+{
+
+    [Required(ErrorMessage = "First Name is required")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last Name is required")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+
+    public ProficiencyLevel ProficiencyLevel { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    // Navigation property for associated schedules
+    public List<Schedule> CustomSchedules { get; set; }
+}
---
+++ UserAPI/Models/ProficiencyLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UserAPI.Models
+{
+    public enum ProficiencyLevel
+    {
+        Beginner,
+        Intermediate,
+        Advanced,
+        // Add other proficiency levels as needed
+    }
+}
+
